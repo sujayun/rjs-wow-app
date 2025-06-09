10dd6cbababcdc28a140b6b00d1e38bedad8c024
Sujay U N <sujayun@yahoo.com>
2025-06-10 00:37:29 +0530

    Wow Cod

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const RjsRutDivVar = ReactDOM.createRoot(document.getElementById('RjsRutDiv'));
+RjsRutDivVar.render(<center>
+	<h1>Wow Working. Welcome to Website</h1>
+</center>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
